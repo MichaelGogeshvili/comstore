746d750978fb597801c8f10c89c5ee6f0c929e8f
Michael <thatisnotthecase@gmail.com>
2017-06-19 09:46:38 +0000

    A

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -5,6 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
   <link rel="stylesheet" href="/css/bootstrap.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.css" >
+  <script
+    src="https://code.jquery.com/jquery-2.2.4.js"   
+    integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
+    crossorigin="anonymous">
+    /script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
 </head>
 <body>
@@ -12,23 +17,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="jumbotron col-md-12">
       <fieldset>
         <legend> За день: <?=$date_posted?></legend>
-        <li> скупка_: ЕЕЕ : на сумму : EEE </li>
-        <li> реализ_: ЕЕЕ : на сумму : EEE </li>
+        <li> <div class="sp">скупка: </div> _______шт : на сумму : ___ <button>Отчет</button></li>
+		<li> <div class="sp">реализация: </div> _______шт : на сумму : ___ <button>Отчет</button></li>
       </fieldset>
     </div>
   </div>
-
   <div class="page-header container">
     <div class="jumbotron row">
       <div class="col-md-12">
         <fieldset class="container">
-          <div class="btn-group-lg col-md-11">
-            <div class="btn btn-lg btn-warning" style="width:190px">
-                  &lt;-Закупка
-            </div>
-            <div class="btn btn-lg btn-success" style="width:190px">
-                  Реализация-&gt;
-            </div>
+          <div class="col-md-11">
+			  <div class="btn-group" style="width:100%;">
+				<div class="btn btn-lg btn-warning" >
+					  &lt;-Закупка
+				</div>
+				<div class="btn btn-lg btn-success" >
+					  Реализация-&gt;
+				</div>
+			  </div>
           </div>
         </fieldset>
       </div>
@@ -37,10 +43,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <strong>{elapsed_time}</strong> seconds. 
       <?php echo 'CI ver. <strong>' . CI_VERSION . '</strong>'  ?></p>
   </div>
-
-
-    
-
-
+<style>
+.btn { 
+	height:100px!important;
+	padding:37px 49px!important;
+	width:50%!important;
+}
+.sp {
+	display:inline-block;
+	width:76px;
+}
+</style>
 </body>
 </html>
